ae258bfd31ded77b8cf25500da4cf20b97ba69a5
Jaryd Hermann <hermannjaryd@gmail.com>
2021-02-05 12:35:26 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,6 +1,9 @@
 def starts_with_a_vowel?(word)
-  string = ("afoot Excellent incredible Absolute unreal Inconceivable octopus")
-    string.grep(/\b[aeiouAEIOU]/)
+  if word.match(/\A+[aeiou]/) == true
+    return true
+  else
+    return false
+  end
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
